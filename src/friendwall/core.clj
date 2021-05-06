27f4888a39154932bfe4619dcf41c0b15f8e1377
skalashynski <sergei.kalashynski@gmail.com>
2021-05-06 09:13:15 +0300

    implementing GET requests

--- src/friendwall/core.clj
+++ src/friendwall/core.clj
@@ -1,6 +1,24 @@
-(ns friendwall.core)
+(ns friendwall.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [compojure.core :refer :all]
+            [compojure.route :as route])
+  (:gen-class))
 
-(defn foo
+(defroutes app
+           (GET "/" [] "<h1>Hello World</h1>")
+           (GET "/friends" [] "<h1>There are no friends yet</h1>")
+           )
+
+(defn app-handler [request]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "<html><body>Hello, There!</body></html>"
+   }
+  )
+
+(defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [& args]
+  (run-jetty app  {:port 80})
+
+  )
